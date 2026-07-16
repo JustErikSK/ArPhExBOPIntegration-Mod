f7e9ae8cbd2bb1e1992cd4814dd2523c402f22dd
justerik <sebo.erik@gmail.com>
2026-07-16 20:54:50 +0200

    added 46 (if I can count correctly lol) empty prepared mob spawn builders for config file

--- src/main/java/net/withrage/arphexbopintegration/config/ArphexBopConfig.java
+++ src/main/java/net/withrage/arphexbopintegration/config/ArphexBopConfig.java
@@ -27,24 +27,554 @@ public class ArphexBopConfig {
 
         builder.push("Mob Specific Spawns");
 
+        // Ant Arsonist
         addMob(
                 builder,
-                "spider_snatcher",
+                "",
                 List.of(
-                        "biomesoplenty:ominous_woods"
+                        ""
                 ),
-                32,
+                1,
                 1,
                 1
         );
 
+        // Beetle Bulwark
         addMob(
                 builder,
-                "spider_jump",
+                "",
                 List.of(
-                        "biomesoplenty:rainforest"
+                        ""
                 ),
-                8,
+                1,
+                1,
+                1
+        );
+
+        // Beetle Tick Mite
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Bloodworm
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Butterfly Bewitcher
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Butterfly Bewitcher Giant
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Centipede Evictor
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Centipede Evictor Larvae
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Centipede Stalker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Crab Constrictor
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Dragonfly Dreadnought
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Fly Festerer
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Hornet Harbinger
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Hornet Harbinger Giant
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Invisible Stalker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Locust Landscourge
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Long Legs
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Long Legs Fly
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Maggot Larvae
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Mantis Mutilator
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Millipede Marauder
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Mosquito Morbidity
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Moth Moontracker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Roach Riverspawn
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Scorpion Striker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Silverfish Spectre
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Sky Stalker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Solifuge Skulker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Ambusher
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Brood
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Flat
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Funnel
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Goliath
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Infestor
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Jump
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Larvae
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Lunger
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Lurker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Obstructer
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Prowler
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Reaper
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Recluse
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Sinker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Spider Snatcher
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Termite Tunneler Worker
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
+                1,
+                1
+        );
+
+        // Venus Flytrap
+        addMob(
+                builder,
+                "",
+                List.of(
+                        ""
+                ),
+                1,
                 1,
                 1
         );
